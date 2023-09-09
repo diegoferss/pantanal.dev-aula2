2c7cdf6594e465ef19cc82bf4444689efb49529f
diego <diegoferss.2019@gmail.com>
2023-09-09 02:32:17 -0300

    first commit

---
+++ src/main/java/dev/pantanal/grupo2/Grupo2Application.java
@@ -0,0 +1,13 @@
+package dev.pantanal.grupo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Grupo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Grupo2Application.class, args);
+	}
+
+}
